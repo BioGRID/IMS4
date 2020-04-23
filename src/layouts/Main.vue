20068de83de16d53ebfe6d4724f350d92ac99358
Chris Stark <starkfree@gmail.com>
2020-04-23 19:07:20 -0400

    fix: removes console.log from Main.vue

--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -38,7 +38,6 @@ export default class MainVue extends Vue {
         // Check to see if route starts with /curation/DatasetView
         // if not, return just the path
         if (!this.$route.path.startsWith( '/curation/DatasetView' )) {
-            console.log(this.$route.path);
             return this.$route.path;
         } else {
             // If it is a dataset path, check to see if path is in the
